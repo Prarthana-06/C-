fb5b679de83326abc3d883b7bba46f9a94ab4850
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:33:05 +0530

    initial commit

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class amm
+{
+    static void Main()
+    {
+
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            int sum = 0, temp = i;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += digit * digit * digit;
+                temp /= 10;
+
+
+            }
+
+            if (sum == i)
+            {
+                Console.WriteLine(i);
+            }
+            
+            
+                
+            
+        }
+    }
+}
