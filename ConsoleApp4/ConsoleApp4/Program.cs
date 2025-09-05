55f37045b87a0b26e647c4649591c07cc2b6419b
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:29:53 +0530

    initial commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class animal
+{
+    public void eat()
+    {
+        Console.WriteLine("hhhh");
+    }
+}
+class dog:animal
+{
+    public void bark()
+    {
+        Console.WriteLine("hjkj");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        dog d=new dog();
+        d.bark();
+
+
+    }
+}
