0972fae1192247618775682da96f2dd69700504b
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:37:12 +0530

    initial commit

---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("drawing a shape");
+
+    }
+    public virtual void Erase()
+    {
+        Console.WriteLine("erase a shape");
+    }
+}
+class circle:shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("draw a circle");
+    }
+}
+class Triangle : shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("draw a triangle");
+    }
+    public override void Erase()
+    {
+        Console.WriteLine("erase a triangle");
+    }
+}
+class square : shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("draw sqare");
+    }
+    public override void Erase()
+    {
+        Console.WriteLine("erasing a square");
+    }
+}
+class program
+{
+    shape s;
+    shape=new Circle();
+
+}
---
+++ ConsoleApp9/ConsoleApp9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a string");
+        string str=Console.ReadLine();
+        Console.WriteLine("asr");
+        for (int i = 0;i<str.Length;i++)
+        {
+            for (int j = 1;j < str.Length - i; j++)
+            {
+                string sub=str.Substring(i,j);
+                Console.WriteLine(sub);
+            }
+        }
+    }
+}
