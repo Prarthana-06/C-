79f559648dff4991673d71675c2d9f345a6dfab2
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:34:21 +0530

    initial commit

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class sum
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a number");
+        int number=Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        int result;
+        while(number != 0)
+        {
+            result = number % 10;
+            sum += result;
+            number = number / 10;
+        }
+        Console.WriteLine("sum"+sum);
+    }
+}
+
