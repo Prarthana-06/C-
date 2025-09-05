69a15680529af079999a3e2fdf7295e78981553f
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:36:00 +0530

    initial commit

---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class hh
+{
+    static void Main()
+    {
+        Console.WriteLine("enter a number");
+        int n=Convert.ToInt32(Console.ReadLine());
+        int lastdigit = n % 10;
+        int firstdigit = n;
+        while (firstdigit >= 10)
+        {
+            firstdigit /= 10;
+                }
+        Console.WriteLine("firstdigit" + firstdigit);
+        Console.WriteLine("last digit" + lastdigit);
+    }
+}
