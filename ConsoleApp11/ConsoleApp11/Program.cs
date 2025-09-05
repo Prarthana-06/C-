55907a7c68eb6b724e5b9386c03d0a6829b8cd03
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:40:02 +0530

    initial commit

---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class pro
+{
+    static void Main()
+
+    {
+        Console.WriteLine("enter a sentence");
+        string sentence = Console.ReadLine();
+        string[]words= sentence.Split(' ');
+        Array.Reverse(words);
+        Console.WriteLine(string.Join(" ", words));
+
+    }
+}
