5c2e3dfbfd9707259322741716bb338d03aab1e5
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:31:46 +0530

    initial commit

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class animal
+{
+    public void eat()
+    {
+        Console.WriteLine("hjhj");
+    }
+}
+class dog : animal
+{
+    public void makesound()
+    {
+        Console.WriteLine("hjjh");
+    }
+}
+class pappy : dog
+{
+    public void run()
+    {
+        Console.WriteLine("run");
+    }
+}
+class program
+{
+    static void Main()
+    {
+        pappy p=new pappy();
+        p.run();
+    }
+}
