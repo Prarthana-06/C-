129842d5bbefe836cde1b131a19995aaf7e3451b
Prarthana Gowda <p6361874024@gmail.com>
2025-09-05 10:27:41 +0530

    initial commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class car
+{
+    public void drive()
+    {
+        Console.WriteLine("car is in driving");
+    }
+
+}
+class enginee : car
+{
+    public void show()
+    {
+        Console.WriteLine("show the enginee");
+    }
+}
+class wheel : enginee
+{
+    public void rotate()
+    {
+        Console.WriteLine("wheel is rotating");
+    }
+}
+class programe
+{
+    static void Main()
+    {
+        wheel w=new wheel();
+        w.rotate();
+    }
+}
