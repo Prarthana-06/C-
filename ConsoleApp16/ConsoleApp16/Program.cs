ec7f7b7f88173f5ae9b8eefed6fe651031bebb51
Prarthana Gowda <p6361874024@gmail.com>
2025-09-19 10:36:48 +0530

    initial commit

---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+interface camera
+{
+    void capture();
+}
+interface printer
+{
+    void print();
+}
+class advance : camera, printer
+{
+    public void capture()
+    {
+        Console.WriteLine(" capture the photo");
+    }
+    public void print()
+    {
+        Console.WriteLine("print the document");
+    }
+}
+    class pro
+{
+    static void Main()
+    {
+        advance a=new advance();
+        a.capture();
+        a.print();
+    }
+}
