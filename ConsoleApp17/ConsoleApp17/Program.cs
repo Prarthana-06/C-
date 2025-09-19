0374f74beb08265e90abfab969815f2051b6ba9f
Prarthana Gowda <p6361874024@gmail.com>
2025-09-19 10:38:04 +0530

    initial commit

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+class Transport
+{
+    public virtual void transport()
+    {
+        Console.WriteLine("go to");
+
+    }
+}
+class truck : Transport
+{
+    public override void transport()
+    {
+        Console.WriteLine("in truck");
+    }
+}
+    class ship : Transport
+    {
+        public override void transport()
+        {
+            Console.WriteLine("in ship");
+        }
+    }
+    class arioplane : Transport
+    {
+        public override void transport()
+        {
+            Console.WriteLine("in airoplane");
+        }
+    }
+    class pro
+    {
+        static void Main()
+        {
+            Transport t = new Transport();
+            t.transport();
+            Transport t1 = new truck();
+            t1.transport();
+            Transport t2 = new ship();
+            t2.transport();
+            Transport t3 = new arioplane();
+            t3.transport();
+        }
+    }
+
+    
