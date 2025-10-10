d149981145a52e13a317ce8157c407a6cc4fc3fb
Prarthana Gowda <p6361874024@gmail.com>
2025-10-10 10:29:38 +0530

    initial commit

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+class Program
+{
+    static void Main()
+    {
+        File.WriteAllText("notes.txt", "File handling in c# is easy !\n lests read this file next");
+        string content = File.ReadAllText("notes.txt");
+        Console.WriteLine(content);
+    }
+}
