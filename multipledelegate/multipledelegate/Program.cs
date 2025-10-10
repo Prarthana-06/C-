42d80ab7114e0873465efb5667e37c82607d5e62
Prarthana Gowda <p6361874024@gmail.com>
2025-10-10 10:48:45 +0530

    initial commit

---
+++ multipledelegate/multipledelegate/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+delegate void Greet();
+class program()
+{
+    static void method1()
+    {
+        Console.WriteLine("hello student");
+    }
+    static void method2()
+    {
+        Console.WriteLine("jjkjk");
+    }
+    static void Main()
+    {
+        Greet d = method1;
+        d += method2;
+        d();
+    }
+}
