d7e9451e313c03537a8ac3ec964130cacff4a549
Prarthana Gowda <p6361874024@gmail.com>
2025-10-10 10:17:50 +0530

    initial commit

---
+++ ConsoleApp25/ConsoleApp25/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Stack<string> stack = new Stack<string>();
+        stack.Push("First");
+        stack.Push("second");
+        Console.WriteLine(stack.Pop());
+    }
+}
