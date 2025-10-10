2a8d893b157deabf0a63567166ca8e5471db835d
Prarthana Gowda <p6361874024@gmail.com>
2025-10-10 10:27:12 +0530

    initial commit

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+
+namespace ConsoleApp18
+{
+    public class Program()
+    {
+        public static void Main()
+        {
+            var result = Console.ReadLine();
+
+            double[] arrayNameVariable = [0, 1, 2, 3, 4, 5]; // Array
+
+
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+
+
+
+
