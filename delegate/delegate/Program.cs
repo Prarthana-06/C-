8832c4b0ad37a93d24a2329fdf50e9f2e8d00869
Prarthana Gowda <p6361874024@gmail.com>
2025-10-10 10:20:43 +0530

    initial commit

---
+++ delegate/delegate/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+delegate void Greet();
+class program
+{
+    static void sayHello()
+    {
+        Console.WriteLine("Hello Students");
+    }
+    static void Main()
+    {
+        Greet greetDelegate = sayHello;
+        greetDelegate();
+    }
+}
